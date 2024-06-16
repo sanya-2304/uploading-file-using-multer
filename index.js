4d908f8b248ef9bbf47530b42ca4e8a9c980bc47
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-06-16 16:43:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const path=require('path')
+const express=require('express')
+const mongoose=require('mongoose')
+const multer=require('multer')
+
+const app=express();
+const PORT=1800;
+
+// const upload=multer({dest: "upload/"})
+const storage=multer.diskStorage({destination:function(req, file, cb){
+    return cb(null, './uploads')
+}, filename:function(req, file, cb){
+    return cb(null, `${Date.now()}-${file.originalname}`)
+}})
+const upload=multer({storage: storage});
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req, res)=>{
+    return res.render("homepage");
+})
+app.post('/upload', upload.single('profileImg'), (req, res)=>{
+    console.log(req.body)
+    console.log(req.file)
+
+    return res.redirect('/')
+})
+app.listen(PORT, ()=>console.log(`server rendering at ${PORT}`))
